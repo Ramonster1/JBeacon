5be434baa6c2bb416a0d3ae6a9fd209404cf4fe6
Ramon Singh <Ramonster1@icloud.com>
2025-04-03 11:18:46 +0100

    Refactor Aeron command error handling and test orchestration
    -Enhanced `AeronOnPollResponseOfferCommand` with detailed logging and error handling for various publication responses, including retries and fatal exceptions.
     -Simplified integration tests by using SubscriberAndPollingTestCoordinator, improving maintainability and readability.

--- src/integrationTest/java/com/jbeacon/aeron/command/AeronOnPollResponseOfferCommandIT.java
+++ src/integrationTest/java/com/jbeacon/aeron/command/AeronOnPollResponseOfferCommandIT.java
@@ -1,8 +1,9 @@
 package com.jbeacon.aeron.command;
 
-import com.jbeacon.aeron.util.AsyncAssertionExecutor;
+import com.jbeacon.aeron.util.SubscriberAndPollingTestCoordinator;
 import com.jbeacon.aeron.util.SubscriptionService;
-import com.jbeacon.poll.UdpPollingService;
+import com.jbeacon.command.OnPollResponseCommand;
+import com.jbeacon.util.PollingTestService;
 import com.jbeacon.util.UdpTestServer;
 import io.aeron.Aeron;
 import io.aeron.Publication;
@@ -17,14 +18,11 @@ import org.junit.jupiter.api.Test;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.SocketException;
-import java.nio.ByteBuffer;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
+import java.util.function.Supplier;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class AeronOnPollResponseOfferCommandIT {
 	private static final String DEFAULT_HOST = "localhost";
@@ -37,25 +35,16 @@ class AeronOnPollResponseOfferCommandIT {
 	private static final String MDC_CONTROL_CHANNEL = String.format("aeron:udp?control-mode=dynamic|control=%s:%d", MDC_HOST, MDC_CONTROL_PORT);
 	private static final String MDC_SUBSCRIPTION_CHANNEL = String.format("aeron:udp?endpoint=localhost:0|control=%s:%d|control-mode=dynamic", MDC_HOST, MDC_CONTROL_PORT);
 
-	private static final int OUT_BUFFER_SIZE = 1;
-	private static final int IN_BUFFER_SIZE = 100;
-	private static final int TEST_ITERATIONS = 10;
-
-
 	private static final UnsafeBuffer BUFFER = new UnsafeBuffer(BufferUtil.allocateDirectAligned(256, 64));
 	@AutoClose
 	private static UdpTestServer testServer;
-	private static InetSocketAddress localhostAddress;
 	@AutoClose
 	private static MediaDriver mediaDriver;
 	@AutoClose
 	private static Aeron aeron;
+	private static SubscriberAndPollingTestCoordinator coordinator;
 
-	private static AsyncAssertionExecutor initSubscriptionExecutor(SubscriptionService subscriptionService, FragmentHandler fragmentHandlerTest) {
-		return new AsyncAssertionExecutor(() -> subscriptionService.startSubscribing(fragmentHandlerTest));
-	}
-
-	private static FragmentHandler createFragmentHandler(UdpTestServer server) {
+	private static FragmentHandler createDefaultFragmentHandlerTest(UdpTestServer server) {
 		return (buf, offset, length, header) -> {
 			final String message = buf.getStringWithoutLengthUtf8(offset, length);
 			assertEquals(new String(server.getData(), UdpTestServer.CHARSET), message);
@@ -70,27 +59,30 @@ class AeronOnPollResponseOfferCommandIT {
 
 		aeron = Aeron.connect(ctx);
 		testServer = new UdpTestServer();
-		localhostAddress = new InetSocketAddress(testServer.getSocket().getLocalAddress(), testServer.getSocket().getLocalPort());
+		InetSocketAddress localhostAddress = new InetSocketAddress(testServer.getSocket().getLocalAddress(), testServer.getSocket().getLocalPort());
 		// This thread should terminate when testServer, which has @AutoClose, is closed
 		Thread serverThread = new Thread(testServer::startServer);
 		serverThread.setDaemon(true);
 		serverThread.start();
+
+		PollingTestService pollingTestService = new PollingTestService(testServer, localhostAddress);
+		coordinator = new SubscriberAndPollingTestCoordinator(pollingTestService);
 	}
 
 	@Test
 	void testOffsetAndLengthValuesProvidedToOffer() throws ExecutionException, InterruptedException, IOException {
+		FragmentHandler fragmentHandlerTest = (buffer, offset, length, header) -> {
+			final String subscriberMsg = buffer.getStringWithoutLengthUtf8(offset, length);
+
+			assertEquals(new String(testServer.getData(), UdpTestServer.CHARSET), subscriberMsg);
+			assertEquals(0, buffer.byteBuffer().position());
+			assertEquals(testServer.getData().length, length);
+		};
+
 		try (Publication publication = aeron.addPublication(DEFAULT_CHANNEL, STREAM_ID)) {
-			try (SubscriptionService subscription = new SubscriptionService(1, DEFAULT_CHANNEL)) {
-
-				FragmentHandler fragmentHandlerTest = (buffer, offset, length, header) -> {
-					final String subscriberMsg = buffer.getStringWithoutLengthUtf8(offset, length);
-
-					assertEquals(new String(testServer.getData(), UdpTestServer.CHARSET), subscriberMsg);
-					assertEquals(0, buffer.byteBuffer().position());
-					assertEquals(testServer.getData().length, length);
-				};
-
-				createServicesAndRunTest(publication, fragmentHandlerTest, subscription);
+			try (SubscriptionService subscription = new SubscriptionService(1, DEFAULT_CHANNEL, fragmentHandlerTest)) {
+				Supplier<OnPollResponseCommand> commandSupplier = () -> new AeronOnPollResponseOfferCommand(publication, BUFFER);
+				coordinator.executeTest(commandSupplier, List.of(subscription));
 			}
 		}
 	}
@@ -98,8 +90,9 @@ class AeronOnPollResponseOfferCommandIT {
 	@Test
 	void testPublicationOfferedAfterPoll() throws Exception {
 		try (Publication publication = aeron.addPublication(DEFAULT_CHANNEL, STREAM_ID)) {
-			try (SubscriptionService subscription = new SubscriptionService(1, DEFAULT_CHANNEL)) {
-				createServicesAndRunTest(publication, subscription);
+			try (SubscriptionService subscription = new SubscriptionService(1, DEFAULT_CHANNEL, createDefaultFragmentHandlerTest(testServer))) {
+				Supplier<OnPollResponseCommand> commandSupplier = () -> new AeronOnPollResponseOfferCommand(publication, BUFFER);
+				coordinator.executeTest(commandSupplier, List.of(subscription));
 			}
 		}
 	}
@@ -107,77 +100,12 @@ class AeronOnPollResponseOfferCommandIT {
 	@Test
 	void testPublishedMessagesForMultiDestinationCastSubscribers() throws Exception {
 		try (Publication publication = aeron.addPublication(MDC_CONTROL_CHANNEL, STREAM_ID)) {
-			try (SubscriptionService subscription1 = new SubscriptionService(1, MDC_SUBSCRIPTION_CHANNEL);
-				 SubscriptionService subscription2 = new SubscriptionService(2, MDC_SUBSCRIPTION_CHANNEL)) {
-				createServicesAndRunTest(publication, subscription1, subscription2);
+			try (SubscriptionService subscription1 = new SubscriptionService(1, MDC_SUBSCRIPTION_CHANNEL, createDefaultFragmentHandlerTest(testServer));
+				 SubscriptionService subscription2 = new SubscriptionService(2, MDC_SUBSCRIPTION_CHANNEL, createDefaultFragmentHandlerTest(testServer))) {
+
+				Supplier<OnPollResponseCommand> commandSupplier = () -> new AeronOnPollResponseOfferCommand(publication, BUFFER);
+				coordinator.executeTest(commandSupplier, List.of(subscription1, subscription2));
 			}
-		}
-	}
-
-	private void createServicesAndRunTest(Publication publication, FragmentHandler fragmentHandler, SubscriptionService... subscribers) throws ExecutionException, InterruptedException, IOException {
-		List<AsyncAssertionExecutor> testExecutors = new ArrayList<>();
-		for (SubscriptionService subscriber : subscribers) {
-			testExecutors.add(initSubscriptionExecutor(subscriber, fragmentHandler));
-		}
-
-		runTest(publication, testExecutors.toArray(AsyncAssertionExecutor[]::new));
-
-		for (SubscriptionService subscriber : subscribers) {
-			subscriber.close();
-		}
-
-		for (AsyncAssertionExecutor subscriberThread : testExecutors) {
-			subscriberThread.waitForCompletionAndAssert();
-		}
-	}
-
-	private void createServicesAndRunTest(Publication publication, SubscriptionService... subscribers) throws ExecutionException, InterruptedException, IOException {
-		createServicesAndRunTest(publication, createFragmentHandler(testServer), subscribers);
-	}
-
-	private void runTest(Publication publication, AsyncAssertionExecutor... subscriberThreads) throws ExecutionException, InterruptedException, IOException {
-		for (AsyncAssertionExecutor subscriberThread : subscriberThreads) {
-			subscriberThread.start();
-		}
-
-		waitForPublicationConnection(publication);
-
-		var pollingService = UdpPollingService.builder()
-				.serverSocketAddress(localhostAddress)
-				.outBuffer(ByteBuffer.allocate(OUT_BUFFER_SIZE))
-				.inBuffer(ByteBuffer.allocate(IN_BUFFER_SIZE))
-				.onPollResponseCommand(new AeronOnPollResponseOfferCommand(publication, BUFFER))
-				.blocks(true)
-				.build();
-
-		// test
-		for (int i = 0; i < TEST_ITERATIONS; i++) {
-			testServer.updateTestServerResponse();
-			pollingService.poll();
-		}
-	}
-
-	/**
-	 * Utility method to wait until the Aeron publication is connected.
-	 * Throws an exception if the connection isn't established within the timeout.
-	 */
-	private void waitForPublicationConnection(Publication publication) throws ExecutionException, InterruptedException {
-		CompletableFuture<Boolean> isSubscribed = CompletableFuture.supplyAsync(() -> {
-			while (true) {
-				if (!publication.isConnected()) {
-					try {
-						TimeUnit.MILLISECONDS.sleep(100);
-					} catch (InterruptedException e) {
-						throw new RuntimeException(e);
-					}
-				} else {
-					return true;
-				}
-			}
-		});
-		isSubscribed.completeOnTimeout(false, 10, TimeUnit.SECONDS);
-		if (!isSubscribed.get()) {
-			fail("Failed to connect to Aeron publication");
 		}
 	}
 }
--- src/main/java/com/jbeacon/aeron/command/AeronOnPollResponseOfferCommand.java
+++ src/main/java/com/jbeacon/aeron/command/AeronOnPollResponseOfferCommand.java
@@ -2,7 +2,10 @@ package com.jbeacon.aeron.command;
 
 import com.jbeacon.command.OnPollResponseCommand;
 import io.aeron.Publication;
+import io.aeron.exceptions.AeronException;
 import org.agrona.DirectBuffer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.nio.ByteBuffer;
 import java.util.function.Consumer;
@@ -21,7 +24,8 @@ import java.util.function.Consumer;
  */
 public record AeronOnPollResponseOfferCommand(Publication publication,
 											  DirectBuffer directBuffer,
-											  Consumer<Long> offerResponseCallback) implements OnPollResponseCommand {
+											  Consumer<Long> responseCallback) implements OnPollResponseCommand {
+	private static final Logger logger = LogManager.getLogger();
 
 	public AeronOnPollResponseOfferCommand(Publication publication, DirectBuffer directBuffer) {
 		this(publication, directBuffer, null);
@@ -42,9 +46,27 @@ public record AeronOnPollResponseOfferCommand(Publication publication,
 	public void execute(ByteBuffer byteBuffer) {
 		directBuffer.wrap(byteBuffer, byteBuffer.position(), byteBuffer.limit());
 
-		final long resultingPosition = publication.offer(directBuffer, 0, byteBuffer.limit());
-		if (offerResponseCallback != null) {
-			offerResponseCallback.accept(resultingPosition);
+		final long response = publication.offer(directBuffer, 0, byteBuffer.limit());
+
+		if (response == Publication.NOT_CONNECTED) {
+			logger.info("Publication is not connected");
+		} else if (response == Publication.BACK_PRESSURED) {
+			logger.info("Publication is back pressured");
+		} else if (response == Publication.ADMIN_ACTION) {
+			logger.info("Publication is in admin action. Attempting a retry");
+
+			// Retry offer(). The action is an operation such as log rotation which is likely to have succeeded by the next retry attempt.
+			execute(byteBuffer);
+		} else if (response == Publication.CLOSED) {
+			logger.warn("Publication is closed");
+
+			throw new AeronException("Publication is closed", AeronException.Category.FATAL);
+		} else if (response == Publication.MAX_POSITION_EXCEEDED) {
+			logger.info("Publication reached max position");
+
+			throw new AeronException("Publication reached max position", AeronException.Category.FATAL);
+		} else {
+			logger.warn("Unknown try claim response: {}", response);
 		}
 	}
 }
